0267743e55fb3b15833efe26a7ee8c8c927ad602
vmandavi <157188845+vmandavi@users.noreply.github.com>
2024-01-30 12:39:12 -0600

    init

---
+++ HelloWorld.py
@@ -0,0 +1,26 @@
+
+# Python script for displaying Hello World
+
+# first python script
+print("----------------------")
+print("Hello, Vidhya!")
+my_string = "Welcome to Python programming!"
+print(my_string)
+if __name__ == '__main__':
+    print("Hello, World!")
+print(1+1)
+x=5
+print(x)
+name: str = 'Vidhya'
+type(name)
+print(type(x))
+print(name)
+
+if name == 'Vidhya':
+    print("True")
+
+mylist = ['S','V', 'R','I']
+myset = {1,2,3,4}
+
+len(myset)
+
